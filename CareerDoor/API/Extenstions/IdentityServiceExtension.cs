66a462d29e5d8392baf2bc38f8f3cde7987a64e0
Garik Hayrapetyan <garhay01@gmail.com>
2021-04-25 13:19:17 +0200

    Added user entiity and modified DataContext

---
+++ CareerDoor/API/Extenstions/IdentityServiceExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Extenstions
+{
+    public static class IdentityServiceExtension
+    {
+        public static IServiceCollection AddIdentityService(this IServiceCollection services, IConfiguration config) {
+
+            return null;
+        }
+    }
+}
--- CareerDoor/Persistence/DataContext.cs
+++ CareerDoor/Persistence/DataContext.cs
@@ -1,10 +1,11 @@
 ﻿using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 
 namespace Persistence
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<AppUser>
     {
         public DbSet<GetTogether> GetTogethers { get; set; }
         public DataContext(DbContextOptions options) : base(options)
