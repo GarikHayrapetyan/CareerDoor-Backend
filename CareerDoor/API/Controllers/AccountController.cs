4b1b63b53f74b3256e4c4bed5341c21d837fc38d
Garik Hayrapetyan <garhay01@gmail.com>
2021-04-25 14:25:52 +0200

    Add user DTOs and account controller

---
+++ CareerDoor/API/Controllers/AccountController.cs
@@ -0,0 +1,55 @@
+﻿using API.DTOs;
+using Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]    
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto) {
+
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+            if (user==null)
+            {
+                return Unauthorized();
+            }
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user,loginDto.Password,false);
+
+            if (result.Succeeded)
+            {
+                return CreateUserObject(user);
+            }
+
+            return Unauthorized();
+        }
+
+        public UserDto CreateUserObject(AppUser user) {
+            return new UserDto
+            {
+                DisplayName = user.DisplayName,
+                Username = user.UserName,
+                Image = null,
+                Token = "Token"
+            };
+        }
+    }
+}
