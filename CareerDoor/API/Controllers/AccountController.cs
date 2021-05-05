88b46c0a85178ab5054c71e9785593b2c3f601f1
Garik Hayrapetyan <garhay01@gmail.com>
2021-05-05 12:19:44 +0200

    Added getting current user endpoint

--- CareerDoor/API/Controllers/AccountController.cs
+++ CareerDoor/API/Controllers/AccountController.cs
@@ -5,14 +5,16 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace API.Controllers
 {
-    //[AllowAnonymous]
+    [AllowAnonymous]
     [ApiController]
     [Route("api/[controller]")]    
     public class AccountController : ControllerBase
@@ -48,6 +50,42 @@ namespace API.Controllers
             return Unauthorized();
         }
 
+        [HttpPost("register")]
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto) {
+            if (await _userManager.Users.AnyAsync(x=>x.Email==registerDto.Email))
+            {
+                return BadRequest("Email taken");
+            }
+            if (await _userManager.Users.AnyAsync(x => x.UserName == registerDto.Username))
+            {
+                return BadRequest("Username taken");
+            }
+
+            var user = new AppUser
+            {
+                DisplayName=registerDto.DisplayName,
+                Email=registerDto.Email,
+                UserName=registerDto.Username
+            };
+
+            var result = await _userManager.CreateAsync(user,registerDto.Password);
+
+            if (result.Succeeded)
+            {
+                return CreateUserObject(user);
+            }
+
+            return BadRequest("Problem registering user");
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<ActionResult<UserDto>> GetCurrentUser() {
+            var user = await _userManager.FindByEmailAsync(User.FindFirstValue(ClaimTypes.Email));
+
+            return CreateUserObject(user);
+        }
+
         public UserDto CreateUserObject(AppUser user) {
             return new UserDto
             {
@@ -57,5 +95,8 @@ namespace API.Controllers
                 Token = _tokenService.CreateToken(user)
             };
         }
+
+
+        
     }
 }
