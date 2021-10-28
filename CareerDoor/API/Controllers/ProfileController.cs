672b872cca921f9def4b0a5d4560d47cbc57f9be
Garik Hayrapetyan <garhay01@gmail.com>
2021-10-28 22:17:47 +0200

    Updated the mapping configuration

--- CareerDoor/API/Controllers/ProfileController.cs
+++ CareerDoor/API/Controllers/ProfileController.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using Application.Profiles;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace API.Controllers
 {
-    public class ProfileController
+    public class ProfileController : BaseApiController
     {
+        [HttpGet("{username}")]
+        public async Task<IActionResult> GetProfile(string username) {
+            return HandleResult(await Mediator.Send(new Details.Query { UserName=username}));
+        }
     }
 }
--- CareerDoor/Application/Core/MappingProfiles.cs
+++ CareerDoor/Application/Core/MappingProfiles.cs
@@ -17,6 +17,9 @@ namespace Application.Core
                 .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName))
                 .ForMember(d => d.Username, o => o.MapFrom(s => s.AppUser.UserName))
                 .ForMember(d => d.Bio, o => o.MapFrom(s => s.AppUser.Bio));
+
+            CreateMap<AppUser, Profiles.Profile>()
+                .ForMember(d=>d.Image, o=>o.MapFrom(s=>s.Photos.FirstOrDefault(x=>x.IsMain).Url));
         }
     }
 }
