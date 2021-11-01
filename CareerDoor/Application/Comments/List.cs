dc9245f423bb446a60a238f576b746007fdcd04e
Garik Hayrapetyan <garhay01@gmail.com>
2021-11-01 15:02:39 +0100

    Added Comments list handler

---
+++ CareerDoor/Application/Comments/List.cs
@@ -0,0 +1,44 @@
+﻿using Application.Core;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Comments
+{
+    public class List
+    {
+        public class Query : IRequest<Result<List<CommentDto>>> {
+            public Guid GetTogetherId { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, Result<List<CommentDto>>>
+        {
+            private readonly DataContext _context;
+            private readonly IMapper _mapper;
+
+            public Handler(DataContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+            public async Task<Result<List<CommentDto>>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var comments = await _context.Comments
+                    .Where(x => x.GetTogether.Id == request.GetTogetherId)
+                    .OrderBy(x => x.CreatedAt)
+                    .ProjectTo<CommentDto>(_mapper.ConfigurationProvider)
+                    .ToListAsync();
+
+                return Result<List<CommentDto>>.Success(comments );
+            }
+        }
+    }
+}
