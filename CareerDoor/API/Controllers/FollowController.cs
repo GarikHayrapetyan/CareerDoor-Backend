f0674f5456d5f953643502a13e5d3124010ef735
Oybek Abdulazizov <s17712@pjwstk.edu.pl>
2021-11-23 19:14:22 +0100

    Added the FollowController

---
+++ CareerDoor/API/Controllers/FollowController.cs
@@ -0,0 +1,15 @@
+﻿using Application.Followers;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class FollowController : BaseApiController
+    {
+        [HttpPost("{username}")]
+        public async Task<IActionResult> Follow(string username)
+        {
+            return HandleResult(await Mediator.Send(new FollowToggle.Command { TargetUsername = username }));
+        }
+    }
+}
