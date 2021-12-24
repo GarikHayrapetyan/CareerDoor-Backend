defb8d1996b3aec4d00523139eebb17dc20178c9
Garik Hayrapetyan <garhay01@gmail.com>
2021-12-24 19:08:24 +0100

    Added Job entity data seeding

--- CareerDoor/Persistence/SeedGetTogether.cs
+++ CareerDoor/Persistence/SeedGetTogether.cs
@@ -104,7 +104,121 @@ namespace Persistence
                 };
                 await context.GetTogethers.AddRangeAsync(getTogethers);
                 await context.SaveChangesAsync();
+            }
 
+            if (!context.Jobs.Any())
+            {
+
+                var jobs = new List<Job> {
+                    new Job{
+                        Title=".Net core",
+                        Type="Full-time",
+                        Description=@"DXC Technology (NYSE: DXC) is the world’s leading independent, end-to-end IT services company,
+                                    helping clients harness the power of innovation to thrive on change. Created by the merger of CSC and the Enterprise Services business
+                                    of Hewlett Packard Enterprise, DXC Technology serves nearly 6,000 private and public sector clients across 70 countries. The company’s 
+                                    technology independence, global talent and extensive partner alliance combine to deliver powerful next-generation IT services and solutions.
+                                    DXC Technology is recognized among the best corporate citizens globally. For more information, visit http://www.dxc.technology/.
+                                    We are looking for experienced and junior candidates, aiming to build a healthy mix of seniority and technology skills in the team.
+                                    Net Developer
+                                    Job Responsibilities Of This Role Will Include
+                                    Develop high quality products and / or sub - systems to agreed deadlines,
+                                    Prepares and executes unit / component tests and carries out unit tests,
+                                    Investigation and resolution(where necessary) of customer issues,
+                                    Participates in code reviews, identifying risks, and escalating issues when necessary,
+                                    Collaborate with other Developers, Analysts, QA to ensure high - quality software,
+                                    Identifies best practice improvements and makes changes as agreed with Team Leader,
+                                    Provides information to the project leader to assist with project planning,
+                                    Provides input on technical direction.
+                                    We Require
+                                    At least 2 years of experience with.NET,
+                                    Good knowledge Microsoft.NET Framework 4.0 / 4.5,
+                                    Experience in ADO.NET, MVC 3 / 4, API rest, Json,
+                                    Base knowledge of Microsoft Visual Studio, TFS and GIT code repository,
+                                    TSQL(SQL Server 2012 and above),
+                                    Good communication skills in English language(reading, writing, speaking),
+                                    BSc / MSc degree in Computer Science, Information Systems or equivalent.",
+                           Date = DateTime.Now,
+                           Company = "PJATK",
+                           Function = "Backend",
+                           Industry = "IT",
+                           Location = "Warsaw",
+                           EmployeeCount = "100+"
+                    },
+
+                    new Job{
+                        Title="Python",
+                        Type="Part-time",
+                        Description=@"DXC Technology (NYSE: DXC) is the world’s leading independent, end-to-end IT services company,
+                                    helping clients harness the power of innovation to thrive on change. Created by the merger of CSC and the Enterprise Services business
+                                    of Hewlett Packard Enterprise, DXC Technology serves nearly 6,000 private and public sector clients across 70 countries. The company’s 
+                                    technology independence, global talent and extensive partner alliance combine to deliver powerful next-generation IT services and solutions.
+                                    DXC Technology is recognized among the best corporate citizens globally. For more information, visit http://www.dxc.technology/.
+                                    We are looking for experienced and junior candidates, aiming to build a healthy mix of seniority and technology skills in the team.
+                                    Net Developer
+                                    Job Responsibilities Of This Role Will Include
+                                    Develop high quality products and / or sub - systems to agreed deadlines,
+                                    Prepares and executes unit / component tests and carries out unit tests,
+                                    Investigation and resolution(where necessary) of customer issues,
+                                    Participates in code reviews, identifying risks, and escalating issues when necessary,
+                                    Collaborate with other Developers, Analysts, QA to ensure high - quality software,
+                                    Identifies best practice improvements and makes changes as agreed with Team Leader,
+                                    Provides information to the project leader to assist with project planning,
+                                    Provides input on technical direction.
+                                    We Require
+                                    At least 2 years of experience with.NET,
+                                    Good knowledge Microsoft.NET Framework 4.0 / 4.5,
+                                    Experience in ADO.NET, MVC 3 / 4, API rest, Json,
+                                    Base knowledge of Microsoft Visual Studio, TFS and GIT code repository,
+                                    TSQL(SQL Server 2012 and above),
+                                    Good communication skills in English language(reading, writing, speaking),
+                                    BSc / MSc degree in Computer Science, Information Systems or equivalent.",
+                           Date = DateTime.Now,
+                           Company = "Boom",
+                           Function = "Backend",
+                           Industry = "IT",
+                           Location = "Krakow",
+                           EmployeeCount = "150"
+                    },
+
+                        new Job{
+                        Title="Angular",
+                        Type="Part-time",
+                        Description=@"DXC Technology (NYSE: DXC) is the world’s leading independent, end-to-end IT services company,
+                                    helping clients harness the power of innovation to thrive on change. Created by the merger of CSC and the Enterprise Services business
+                                    of Hewlett Packard Enterprise, DXC Technology serves nearly 6,000 private and public sector clients across 70 countries. The company’s 
+                                    technology independence, global talent and extensive partner alliance combine to deliver powerful next-generation IT services and solutions.
+                                    DXC Technology is recognized among the best corporate citizens globally. For more information, visit http://www.dxc.technology/.
+                                    We are looking for experienced and junior candidates, aiming to build a healthy mix of seniority and technology skills in the team.
+                                    Net Developer
+                                    Job Responsibilities Of This Role Will Include
+                                    Develop high quality products and / or sub - systems to agreed deadlines,
+                                    Prepares and executes unit / component tests and carries out unit tests,
+                                    Investigation and resolution(where necessary) of customer issues,
+                                    Participates in code reviews, identifying risks, and escalating issues when necessary,
+                                    Collaborate with other Developers, Analysts, QA to ensure high - quality software,
+                                    Identifies best practice improvements and makes changes as agreed with Team Leader,
+                                    Provides information to the project leader to assist with project planning,
+                                    Provides input on technical direction.
+                                    We Require
+                                    At least 2 years of experience with.NET,
+                                    Good knowledge Microsoft.NET Framework 4.0 / 4.5,
+                                    Experience in ADO.NET, MVC 3 / 4, API rest, Json,
+                                    Base knowledge of Microsoft Visual Studio, TFS and GIT code repository,
+                                    TSQL(SQL Server 2012 and above),
+                                    Good communication skills in English language(reading, writing, speaking),
+                                    BSc / MSc degree in Computer Science, Information Systems or equivalent.",
+                           Date = DateTime.Now,
+                           Company = "React",
+                           Function = "Frontend",
+                           Industry = "IT",
+                           Location = "London",
+                           EmployeeCount = "520"
+                    }
+
+                };
+
+                await context.Jobs.AddRangeAsync(jobs);
+                await context.SaveChangesAsync();
             }
         }
     }
