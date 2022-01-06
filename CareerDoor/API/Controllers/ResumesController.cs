96a8dfd2c19099c71b6972f7b3b6e2febf2d7973
Garik Hayrapetyan <garhay01@gmail.com>
2022-01-06 21:03:51 +0100

    Added ResumesController Add end-point

---
+++ CareerDoor/API/Controllers/ResumesController.cs
@@ -0,0 +1,15 @@
+﻿using Application.Resumes;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class ResumesController :BaseApiController
+    {
+        [HttpPost]
+        public async Task<IActionResult> Add([FromForm] Add.Commmand commmand)
+        {
+            return HandleResult(await Mediator.Send(commmand));
+        }
+    }
+}
